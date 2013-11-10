b1921e9c17b4d2e31d2cce5c6c97a3a510cbbe8a
El1t <3lli7e@gmail.com>
2013-11-09 22:05:27 -0500

    Marker Beta

---
+++ src/atl/phillython/createMarker.java
@@ -0,0 +1,19 @@
+package atl.phillython;
+
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class createMarker
+{
+	public MarkerOptions choose(double latitude, double longitude, String name)
+	{
+		BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.historical_marker);
+		return new MarkerOptions()
+			.position(new LatLng(latitude, longitude))
+			.title(name)
+			.flat(true)
+			.icon(icon);
+	}
+}
