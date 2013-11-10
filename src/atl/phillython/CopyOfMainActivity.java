5b5747efac4353bbc66bb685d440cbfbb4c72b88
lexical_enumeration <bzhang.2003@gmail.com>
2013-11-09 19:40:35 -0500

    Marker adding via longpress

---
+++ src/atl/phillython/CopyOfMainActivity.java
@@ -0,0 +1,88 @@
+package atl.phillython;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class CopyOfMainActivity extends Activity {
+    private GoogleMap googleMap;
+    
+    public class onMapLongClickListener implements OnMapLongClickListener{
+		int markers = 0;
+		@Override
+		public void onMapLongClick(LatLng arg0) {
+			 // create marker
+			markers ++;
+            MarkerOptions marker = new MarkerOptions().position(arg0).title("marker no " + markers);
+            // adding marker
+            googleMap.addMarker(marker);
+		}
+    }
+    
+    private void initializeClickListener(){
+    	googleMap.setOnMapLongClickListener(new onMapLongClickListener());
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+ 
+        try {
+            // Loading map
+            initilizeMap();
+            initializeClickListener();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
+    }
+ 
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+            // latitude and longitude
+            double latitude = 39.920955;
+            double longitude = -75.183258;
+             
+            // create marker
+            MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Hello Maps ");
+             
+            // adding marker
+            googleMap.addMarker(marker);
+        }
+    }
+ 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
+ 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
